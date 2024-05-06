0aac42badea4df30dcf465dd51c5044400cc77ea
Marcus Lindblom <m@marcuslindblom.com>
2024-05-06 15:12:11 +0200

    Adds Sentry and Spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
 import lit from "@astrojs/lit";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,10 +20,18 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), mdx(), lit()],
+  integrations: [sentry(
+    {
+      dsn: "https://eb16c95c7baebd840c800befde6799bb@o429298.ingest.us.sentry.io/4507209574318080",
+      sourceMapsUploadOptions: {
+        project: "sgf",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }
+  ), spotlightjs(), tailwind(), sitemap(), lit()],
   output: "server",
   vite: {
-    plugins:[basicSsl()],
+    plugins: [basicSsl()],
     server: {
       https: true
     },
