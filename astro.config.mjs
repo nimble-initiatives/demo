f9efcf931c9b9e9cec7b38039b0fb8df3cf1cfd5
Marcus Lindblom <m@marcuslindblom.com>
2024-05-08 10:17:38 +0200

    Adds Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import lit from "@astrojs/lit";
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,15 +22,13 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://lexingtonthemes.com',
-  integrations: [sentry(
-    {
-      dsn: "https://eb16c95c7baebd840c800befde6799bb@o429298.ingest.us.sentry.io/4507209574318080",
-      sourceMapsUploadOptions: {
-        project: "sgf",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
+  integrations: [sentry({
+    dsn: "https://eb16c95c7baebd840c800befde6799bb@o429298.ingest.us.sentry.io/4507209574318080",
+    sourceMapsUploadOptions: {
+      project: "sgf",
+      authToken: process.env.SENTRY_AUTH_TOKEN
     }
-  ), spotlightjs(), tailwind(), sitemap(), lit()],
+  }), spotlightjs(), tailwind(), sitemap(), lit()],
   output: "server",
   vite: {
     plugins: [basicSsl()],
@@ -39,4 +39,5 @@ export default defineConfig({
       noExternal: ['@strifeapp/strife']
     }
   },
+  adapter: vercel()
 });
